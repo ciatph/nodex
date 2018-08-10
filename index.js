cda3125440192bfb670d3bead48708a8bd2be64b
ciatph <ciat.ph@gmail.com>
2018-08-11 02:08:29 +0700

    Added Procfile. Added test script.

--- index.js
+++ index.js
@@ -3,8 +3,9 @@
  * Creates a server and write a plain text output on web
  */
 const http = require('http');
+const PORT = process.env.PORT || 5000
 
 http.createServer(function(req, res){
     res.writeHead(200, {'Content-Type': 'text/plain'});
     res.end('Hello, world!');
-}).listen(8080);
+}).listen(PORT, () => console.log(`listening on ${PORT}`));
